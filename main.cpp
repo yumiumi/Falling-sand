113804bd8f9fc6d3160dbb7eb183f7f99338ebdb
Yumi <galkina1611@gmail.com>
2025-07-03 20:23:12 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+﻿#include "raylib.h"
+#include "raymath.h"
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+const int scr_w = 700;
+const int scr_h = 900;
+
+const int map_w = 30;
+const int map_h = 30;
+
+const int cell_size = 20;
+const int grid_thickness = 1;
+
+
+void render() {
+	BeginDrawing();
+	ClearBackground(BLACK);
+	EndDrawing();
+}
+
+int main() {
+
+	InitWindow(scr_w, scr_h, "Falling sand");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+		render();
+	}
+	return 0;
+}
